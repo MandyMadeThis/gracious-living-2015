fa5cdc32d3dd915e63926adf87bb25f5d70500d9
Mandy <hi@mandymadethis.com>
2015-06-08 22:44:41 -0400

    more customizations

--- gracious_living_2015/framework/views/ethos/wp-header.php
+++ gracious_living_2015/framework/views/ethos/wp-header.php
@@ -13,6 +13,7 @@
   <?php x_get_view( 'global', '_slider-above' ); ?>
 
   <header class="<?php x_masthead_class(); ?>" role="banner">
+  
     <?php if (  !is_front_page() ) { x_get_view( 'ethos', '_post', 'carousel' ); } ?>
     <?php x_get_view( 'global', '_topbar' ); ?>
     <?php x_get_view( 'global', '_navbar' ); ?>
--- gracious_living_2015/functions.php
+++ gracious_living_2015/functions.php
@@ -24,25 +24,16 @@ add_filter( 'upload_mimes', 'cc_mime_types' );
 // Allow shortcodes in widgets
 add_filter('widget_text', 'do_shortcode');
 
-//Add a signup form under header
-function add_custom_content_after_header(){ ?>
-  <div class="sign-up-form-wrapper">
-    <div class="inner-sign-up-wrapper">
-      <h4>Never miss a post</h4>
-      <form method="post" id="x-subscribe-form-9512" class="x-subscribe-form x-subscribe-form-9512" data-x-email-confirm="Message" data-x-email-message="Thank you! You are now subscribed and will be updated when I post new content.">
-        <input type="hidden" name="x_subscribe_form[id]" value="9512">
-        <fieldset>
-          <input type="text" name="x_subscribe_form[first-name]" id="x_subscribe_form_first_name" placeholder="First Name" required="">
-        </fieldset>
-        <fieldset>
-          <input type="email" name="x_subscribe_form[email]" id="x_subscribe_form_email" placeholder="Email" required="">
-        </fieldset>
-        <fieldset>
-          <input type="submit" name="x_subscribe_form[submit]" class="submit x-btn x-btn-flat x-btn-square" value="Sign Me Up!">
-        </fieldset>
-      </form>
-    </div>
-  </div>
-<?php }
-
-add_action('x_before_view_ethos__landmark-header', 'add_custom_content_after_header');
+// Add category classes on single posts
+add_filter('body_class','add_category_to_single');
+function add_category_to_single($classes, $class) {
+  if (is_single() ) {
+    global $post;
+    foreach((get_the_category($post->ID)) as $category) {
+      // add category slug to the $classes array
+      $classes[] = $category->category_nicename;
+    }
+  }
+  // return the $classes array
+  return $classes;
+}
