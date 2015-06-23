f42c395ca68457816ebbac15c59cb1ddf358edf9
Mandy <hi@mandymadethis.com>
2015-06-23 03:50:56 -0400

    site is live for first day

--- gracious_living_2015/functions.php
+++ gracious_living_2015/functions.php
@@ -24,16 +24,18 @@ add_filter( 'upload_mimes', 'cc_mime_types' );
 // Allow shortcodes in widgets
 add_filter('widget_text', 'do_shortcode');
 
+
 // Add category classes on single posts
-add_filter('body_class','add_category_to_single');
-function add_category_to_single($classes, $class) {
-  if (is_single() ) {
-    global $post;
-    foreach((get_the_category($post->ID)) as $category) {
-      // add category slug to the $classes array
-      $classes[] = $category->category_nicename;
-    }
-  }
-  // return the $classes array
-  return $classes;
-}
+// add_filter('body_class','add_category_to_single');
+
+// function add_category_to_single($classes, $class) {
+//   if (is_single() ) {
+//     global $post;
+//     foreach((get_the_category($post->ID)) as $category) {
+//       // add category slug to the $classes array
+//       $classes[] = $category->category_nicename;
+//     }
+//   }
+//   // return the $classes array
+//   return $classes;
+// }
