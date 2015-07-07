77109f2662798d7f5fd861a92edd1bfb60c626a6
Mandy <hi@mandymadethis.com>
2015-07-07 08:52:19 -0400

    added Events to RSS Feed and featured images to RSS

--- gracious_living_2015/functions.php
+++ gracious_living_2015/functions.php
@@ -24,5 +24,62 @@ add_filter( 'upload_mimes', 'cc_mime_types' );
 // Allow shortcodes in widgets
 add_filter('widget_text', 'do_shortcode');
 
+//filter categories on blog index 
+// function blog_index_categories( $query ) {
+//  if ( $query->is_home() && $query->is_main_query() ) {
+//  $query->set( 'cat', '798, 435, 812, 795, 457, 439');
+//  }
+// }
+// add_action( 'pre_get_posts', 'blog_index_categories' );
+
+// Add Events to RSS Feed
+function add_events_to_rss_feed( $args ) {
+  if ( isset( $args['feed'] ) && !isset( $args['post_type'] ) )
+    $args['post_type'] = array('post', 'tribe_events');
+  return $args;
+}
+ 
+add_filter( 'request', 'add_events_to_rss_feed' );
+
+
+// Add Tribe Event Namespace
+add_action( 'rss2_ns', 'events_rss2_namespace' );
+ 
+function events_rss2_namespace() {
+    echo 'xmlns:ev="http://purl.org/rss/2.0/modules/event/"'."\n";
+}
+ 
+// Add Event Dates to RSS Feed
+add_action('rss_item','tribe_rss_feed_add_eventdate');
+add_action('rss2_item','tribe_rss_feed_add_eventdate');
+add_action('commentsrss2_item','tribe_rss_feed_add_eventdate');
+ 
+function tribe_rss_feed_add_eventdate() {
+  if ( ! tribe_is_event() ) return;
+  ?>
+  <ev:tribe_event_meta xmlns:ev="Event">
+  <?php if (tribe_get_start_date() !== tribe_get_end_date() ) { ?>
+ 
+    <ev:startdate><?php echo tribe_get_start_date(); ?></ev:startdate>
+    <ev:enddate><?php echo tribe_get_end_date(); ?></ev:enddate>
+ 
+  <?php } else { ?>
+ 
+    <ev:startdate><?php echo tribe_get_start_date(); ?></ev:startdate>
+ 
+  <?php } ?>
+  </ev:tribe_event_meta>
+ 
+<?php }
+
+// Tell The Events Calendar not to interfere with the RSS post order when events are included.
+add_action( 'pre_get_posts', 'custom_teardown_tribe_order_filter', 60 );
+ 
+function custom_teardown_tribe_order_filter() {
+ if ( is_feed() ) remove_filter( 'posts_orderby', array( 'Tribe__Events__Query', 'posts_orderby' ), 10, 2 );
+}
+
+
+
 
 
