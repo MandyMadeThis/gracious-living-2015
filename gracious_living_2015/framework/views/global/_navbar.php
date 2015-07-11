1c851291fefaef1e28e95677bb1fad8ac755e25f
Mandy <hi@mandymadethis.com>
2015-07-11 16:52:28 -0400

    lots of menu fixes including the signup form

--- gracious_living_2015/framework/views/global/_navbar.php
+++ gracious_living_2015/framework/views/global/_navbar.php
@@ -1,82 +1,61 @@
 <?php
-
 // =============================================================================
 // VIEWS/GLOBAL/_NAVBAR.PHP
 // -----------------------------------------------------------------------------
 // Outputs the navbar.
 // =============================================================================
-
 $navbar_position = x_get_navbar_positioning();
 $logo_nav_layout = x_get_logo_navigation_layout();
 $is_one_page_nav = x_is_one_page_navigation();
-
 ?>
-
 <?php if ( ( $navbar_position == 'static-top' || $navbar_position == 'fixed-top' || $is_one_page_nav ) && $logo_nav_layout == 'stacked' ) : ?>
-
-  <div class="x-logobar">
-    <div class="x-logobar-inner">
+<div class="x-logobar">
+  <div class="x-logobar-inner">
+    <div class="x-container max width">
+      <?php x_get_view( 'global', '_brand' ); ?>
+    </div>
+  </div>
+</div>
+<div class="x-navbar-wrap">
+  <div class="<?php x_navbar_class(); ?>">
+    <div class="x-navbar-inner">
+      <div class="x-container max width">
+        <?php x_get_view( 'global', '_nav', 'primary' ); ?>
+      </div>
+    </div>
+    <div class="sign-up-form-wrapper">
+      <form method="post" id="x-subscribe-form-9512" class="x-subscribe-form x-subscribe-form-9512" data-x-email-confirm="Message" data-x-email-message="Thank you! You are now subscribed and will be updated when I post new content.">
+        <input type="hidden" name="x_subscribe_form[id]" value="9512">
+        <ul class="signup-inline">
+          <li class="callout-phrase">Receive free weekly recipes + Inspirations</li>
+          <li><input type="text" name="x_subscribe_form[first-name]" id="x_subscribe_form_first_name" placeholder="Nickname" required></li>
+          <li><input type="email" name="x_subscribe_form[email]" id="x_subscribe_form_email" placeholder="Email" required></li>
+          <li><input type="submit" name="x_subscribe_form[submit]" class="submit x-btn x-btn-flat x-btn-square" value="Oh Yeah!"></li>
+        </ul>
+      </form>
+    </div>
+  </div>
+</div>
+<?php else : ?>
+<div class="x-navbar-wrap">
+  <div class="<?php x_navbar_class(); ?>">
+    <div class="x-navbar-inner">
       <div class="x-container max width">
         <?php x_get_view( 'global', '_brand' ); ?>
+        <?php x_get_view( 'global', '_nav', 'primary' ); ?>
       </div>
     </div>
-  </div>
-
-  <div class="x-navbar-wrap">
-    <div class="<?php x_navbar_class(); ?>">
-      <div class="x-navbar-inner">
-        <div class="x-container max width">
-          <?php x_get_view( 'global', '_nav', 'primary' ); ?>
-        </div>
-      </div>
-      <div class="sign-up-form-wrapper">
-        <div class="inner-sign-up-wrapper">
-          <h4>Never miss a post</h4>
-          <form method="post" id="x-subscribe-form-9512" class="x-subscribe-form x-subscribe-form-9512" data-x-email-confirm="Message" data-x-email-message="Thank you! You are now subscribed and will be updated when I post new content.">
-            <input type="hidden" name="x_subscribe_form[id]" value="9512">
-            <fieldset>
-              <input type="text" name="x_subscribe_form[first-name]" id="x_subscribe_form_first_name" placeholder="First Name" required="">
-            </fieldset>
-            <fieldset>
-              <input type="email" name="x_subscribe_form[email]" id="x_subscribe_form_email" placeholder="Email" required="">
-            </fieldset>
-            <fieldset>
-              <input type="submit" name="x_subscribe_form[submit]" class="submit x-btn x-btn-flat x-btn-square" value="Sign Me Up!">
-            </fieldset>
-          </form>
-        </div>
-      </div>
+    <div class="sign-up-form-wrapper">
+      <form method="post" id="x-subscribe-form-9512" class="x-subscribe-form x-subscribe-form-9512" data-x-email-confirm="Message" data-x-email-message="Thank you! You are now subscribed and will be updated when I post new content.">
+        <input type="hidden" name="x_subscribe_form[id]" value="9512">
+        <ul class="signup-inline">
+          <li class="callout-phrase">Receive free weekly recipes + Inspirations</li>
+          <li><input type="text" name="x_subscribe_form[first-name]" id="x_subscribe_form_first_name" placeholder="Nickname" required></li>
+          <li><input type="email" name="x_subscribe_form[email]" id="x_subscribe_form_email" placeholder="Email" required></li>
+          <li><input type="submit" name="x_subscribe_form[submit]" class="submit x-btn x-btn-flat x-btn-square" value="Oh Yeah!"></li>
+        </ul>
+      </form>
     </div>
   </div>
-
-<?php else : ?>
-
-  <div class="x-navbar-wrap">
-    <div class="<?php x_navbar_class(); ?>">
-      <div class="x-navbar-inner">
-        <div class="x-container max width">
-          <?php x_get_view( 'global', '_brand' ); ?>
-          <?php x_get_view( 'global', '_nav', 'primary' ); ?>
-        </div>
-      </div>
-      <div class="sign-up-form-wrapper">
-        <div class="inner-sign-up-wrapper">
-          <h4>Never miss a post</h4>
-          <form method="post" id="x-subscribe-form-9512" class="x-subscribe-form x-subscribe-form-9512" data-x-email-confirm="Message" data-x-email-message="Thank you! You are now subscribed and will be updated when I post new content.">
-            <input type="hidden" name="x_subscribe_form[id]" value="9512">
-            <fieldset>
-              <input type="text" name="x_subscribe_form[first-name]" id="x_subscribe_form_first_name" placeholder="First Name" required="">
-            </fieldset>
-            <fieldset>
-              <input type="email" name="x_subscribe_form[email]" id="x_subscribe_form_email" placeholder="Email" required="">
-            </fieldset>
-            <fieldset>
-              <input type="submit" name="x_subscribe_form[submit]" class="submit x-btn x-btn-flat x-btn-square" value="Sign Me Up!">
-            </fieldset>
-          </form>
-        </div>
-      </div>
-    </div>
-  </div>
-
+</div>
 <?php endif; ?>
--- gracious_living_2015/functions.php
+++ gracious_living_2015/functions.php
@@ -24,6 +24,21 @@ add_filter( 'upload_mimes', 'cc_mime_types' );
 // Allow shortcodes in widgets
 add_filter('widget_text', 'do_shortcode');
 
+// Add category body class
+function add_category_to_single($classes) {
+  if (is_single() ) {
+    global $post;
+    foreach((get_the_category($post->ID)) as $category) {
+      // add category slug to the $classes array
+      $classes[] = $category->category_nicename;
+    }
+  }
+  // return the $classes array
+  return $classes;
+}
+
+add_filter('body_class','add_category_to_single');
+
 //filter categories on blog index 
 // function blog_index_categories( $query ) {
 //  if ( $query->is_home() && $query->is_main_query() ) {
